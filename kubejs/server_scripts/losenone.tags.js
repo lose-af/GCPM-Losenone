347f66212e86a5d4bd576006bcb2ce3903fde57a
Nixieboluo <me@sagirii.me>
2024-10-16 20:07:14 +0800

    add tags for compact

---
+++ kubejs/server_scripts/losenone.tags.js
@@ -0,0 +1,14 @@
+// Item tags
+ServerEvents.tags('item', (event) => {
+	// For Immersive Engineering
+	// Coal Coke -> Coke
+	event.add('forge:gems/coke', '#forge:coal_coke');
+	event.add('forge:storage_blocks/coke', '#forge:storage_blocks/coal_coke');
+
+	// Use GTCEu Bauxite tags for Raw Aluminum
+	// (Take care of differences between "aluminium" and "aluminum")
+	event.add('forge:raw_materials/bauxite', 'immersiveengineering:raw_aluminum');
+	event.add('forge:storage_blocks/raw_bauxite', 'immersiveengineering:raw_block_aluminum');
+	event.remove('forge:raw_materials/aluminum', 'immersiveengineering:raw_aluminum');
+	event.remove('forge:storage_blocks/raw_aluminum', 'immersiveengineering:raw_block_aluminum');
+});
